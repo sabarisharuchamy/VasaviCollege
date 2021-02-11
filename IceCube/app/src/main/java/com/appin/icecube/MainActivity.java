bbb0a2c63bf9c8bd4359734a74f4dcd93d130928
sabarisharuchamy <appin.sabarish@gmail.com>
2021-02-11 17:57:38 +0530

    added examples

--- IceCube/app/src/main/java/com/appin/icecube/MainActivity.java
+++ IceCube/app/src/main/java/com/appin/icecube/MainActivity.java
@@ -1,32 +1,79 @@
 package com.appin.icecube;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-ListView listView;
-String data[];
+Button b1;
+TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=findViewById(R.id.listView);
-        data = getResources().getStringArray(R.array.tech);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,android.R.id.text1,data);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Log.d("onCreate","onCreate Event Invoked");
+        b1=findViewById(R.id.button);
+        t1=findViewById(R.id.textView);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String value=adapter.getItem(position);
-                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+//                intent.putExtra("name","Sabari");
+//                intent.putExtra("designation","software developer");
+//                startActivity(intent);
+                startActivityForResult(intent,2);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==2){
+            String message = data.getStringExtra("MESSAGE");
+t1.setText(message);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("onStart","onStart Event Invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("onPause","onPause Event Invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume","onResume Event Invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("onRestart","onRestart Event Invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("onStop","onStop Event Invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("onDestroy","onDestroy Event Invoked");
     }
 }
