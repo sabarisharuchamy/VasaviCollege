16bdbd85efab462040740d5db32887a1780a0c22
sabarisharuchamy <appin.sabarish@gmail.com>
2021-03-18 21:53:10 +0530

    Adding Alarm Manager (Alarm - full song) Ex

---
+++ AndroidAlarmBroadcastService/app/src/main/java/com/journaldev/androidalarmbroadcastservice/PlaySongService.java
@@ -0,0 +1,45 @@
+package com.journaldev.androidalarmbroadcastservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class PlaySongService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    public PlaySongService() {
+    }
+
+    // Return the communication channel to the service.
+    @Override
+    public IBinder onBind(Intent intent){
+        // This service is unbounded
+        // So this method is never called.
+        return null;
+    }
+
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        // Create MediaPlayer object, to play your song.
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.vathicoming);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        // Play song.
+        mediaPlayer.start();
+
+        return START_STICKY;
+    }
+
+    // Destroy
+    @Override
+    public void onDestroy() {
+        // Release the resources
+        mediaPlayer.release();
+        super.onDestroy();
+    }
+}
